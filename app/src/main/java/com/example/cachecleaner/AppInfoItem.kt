8676dd61219e1dd4920d4cf7f42119125401f639
engbaraafd-maker <eng.baraafd@gmail.com>
2026-09-25 07:22:41 +0300

    Create AppInfoItem.kt

---
+++ app/src/main/java/com/example/cachecleaner/AppInfoItem.kt
@@ -0,0 +1,9 @@
+package com.example.cachecleaner
+
+import android.graphics.drawable.Drawable
+
+data class AppInfoItem(
+    val packageName: String,
+    val label: String,
+    val icon: Drawable
+)
